7e219444d10c2869b0d0f935a8b4c422ae565893
monishsoni7 <monishsoni3@gmail.com>
2025-07-27 23:46:38 +0530

    Fix vite build permissions on render last

--- backend/src/index.js
+++ backend/src/index.js
@@ -68,12 +68,13 @@ app.use("/api/albums",albumRoutes);
 app.use("/api/stats",statsRoutes);
 
 // Always serve frontend unless explicitly in development
-if (process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
   });
 }
+
 
 
 app.use((err,req,res,next)=>{
